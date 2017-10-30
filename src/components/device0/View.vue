a2d3e686390d0ce0721cc23dafca9e28c0461dbd
Ian <iamgjek@gmail.com>
2017-10-30 19:04:38 +0800

    Update keyboard pix

--- src/components/device0/View.vue
+++ src/components/device0/View.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="device0View" class="device device0 col-md-9">
+  <div id="device0View" class="device device0 row">
     <section id="device1Setting1" class="setting setting0">
-      <h1>{{ msg + ' 0-1' }}</h1>
-      <h2>Essential Links</h2>
+      <!-- <h1>{{ msg + ' 0-1' }}</h1> -->
+      <!-- <h2>Essential Links</h2> -->
+      <img src="../../assets/images/keyboard.png" class="img-fluid">
     </section>
     <section id="device1Setting2" class="setting setting1 d-none">
       <h1>{{ msg + ' 0-2' }}</h1>
@@ -20,7 +21,7 @@
 export default {
   data () {
     return {
-      msg: 'device0View'
+      msg: 'Keyboard'
     }
   }
 }
